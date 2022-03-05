36d3cdcbc245d787500e51d281238f1e28853f89
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-03-05 13:55:49 +0000

    Styled-components and Tailwindcss

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from "./styles";
+
+
+export function Card() {
+  return (
+    <Container className="text-4xl">
+      <Title>
+        Título
+      </Title>
+    </Container>
+  )
+}
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: blue;
+  padding: 20px;
+`
+
+  export const Title = styled.h1`
+  color: red;
+`;

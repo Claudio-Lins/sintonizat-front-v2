7e8e43475410fca524fcb4bff017eb0d4dcc0ce6
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-03-24 08:40:52 +0000

    .

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,7 +4,7 @@ import { Container, Form } from "./styles";
 export function Card() {
   return (
     <Container className="text-4xl">
-      <p>
+      <p className=" text-slate-600">
         Título
       </p>
     </Container>
